80b0cba90c1838e251fb7b129c4d17989b0ca7db
Jeff Kesselman <jpkessel@purdue.edu>
2023-03-13 14:18:46 -0400

    Initial commit

---
+++ TOM/Program.fs
@@ -0,0 +1,20 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open Fable.Remoting.DotnetClient
+type IGreetingApi = {
+  greet : string -> Async<string>
+}
+
+type IGoodbyeApi = {
+  goodbye : string -> Async<string>
+}
+
+let server =
+    // Also note the base URI is no longer optional.
+    Remoting.createApi "https:/localhost:8080" 
+    |> Remoting.buildProxy<IGreetingApi>
+
+let unitAsync = 
+    async {
+        let! greeting = server.greet "Alfred"
+        printfn $"{greeting}"
+    }
---
+++ TOMServo/Program.fs
@@ -0,0 +1,49 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open Suave
+open Fable.Remoting.Server
+open Fable.Remoting.Suave
+
+type IGreetingApi = {
+  greet : string -> Async<string>
+}
+
+type IGoodbyeApi = {
+  goodbye : string -> Async<string>
+}
+
+// Create the WebPart from the musicStore value
+let greetingApi = {
+  greet = fun name ->
+    async {
+      let greeting = sprintf "Hello, %s" name
+      return greeting
+    }
+}
+
+let goodbyeApi = {
+  goodbye = fun name ->
+    async {
+      let greeting = sprintf "Hello, %s" name
+      return greeting
+    }
+}
+
+let webApp1:WebPart =
+  Remoting.createApi()
+    |> Remoting.fromValue greetingApi
+    |> Remoting.buildWebPart
+
+let webApp2:WebPart =
+  Remoting.createApi()
+    |> Remoting.fromValue greetingApi
+    |> Remoting.buildWebPart
+let webApps = choose([webApp1; webApp2])
+
+// Expose the implementation as a HTTP service
+let webApp:WebPart =
+  Remoting.createApi()
+    |> Remoting.fromValue greetingApi
+    |> Remoting.buildWebPart
+
+
+startWebServer defaultConfig webApp
